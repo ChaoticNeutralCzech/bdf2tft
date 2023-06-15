ea01851dc5c161c50cbb86efbf24ad1bf737bb9a
ChaoticNeutralCzech <92913243+ChaoticNeutralCzech@users.noreply.github.com>
2023-06-15 20:47:51 +0200

    Uploaded first version of the code

---
+++ bdf2tft.py
@@ -0,0 +1,116 @@
+import sys          #Allows reading CLI arguments
+import re
+
+if len(sys.argv) < 2:
+    print("USAGE: " + sys.argv[0] + " BDF filename without extension")
+    exit
+fontname = sys.argv[1]
+
+bdf = open(fontname + ".bdf", 'r')
+lines = bdf.readlines()
+
+
+#LUT: each char can reference another in its bitmap, or have forced width, or have lines added on top
+#To: Use bitmap from char ... / RL: Remove ... Lines / FW: Force width to
+#compressCharTo = 
+#compressCharRL = 
+#compressCharFW = 
+downhang = 1
+
+bitmapData = "const uint8_t " + fontname + "_Bitmap[] PROGMEM = {\r\n\t// Bitmap Data:"     
+#Cache for the first array in the H file
+indexData = "const GFXglyph " + fontname + "_Glyphs[] PROGMEM = {\r\n	// bitmap  wid  hei  x    x    y\r\n  // Offset, th , ght, Adv, Ofs, Ofs"      
+#Cache for the second array in the H file
+
+onBitmapLine = 0
+getpixsize = re.compile(r'PIXEL_SIZE (\-?\d+)')
+getcharnum = re.compile(r'ENCODING (\-?\d+)')
+getName = re.compile(r'STARTCHAR (.*)')
+getWidth = re.compile(r'DWIDTH (\-?\d+) .*')
+getBBX = re.compile(r'BBX (\-?\d+) (\-?\d+) (\-?\d+) (\-?\d+)')
+startBitmap = re.compile(r'BITMAP')
+endBitmap = re.compile(r'END')
+binaryIndex = 0
+
+charnum = 0
+oldcharnum = 0
+firstcharnum = 0
+charName = ""
+advance = 0
+BBX_w = 0
+BBX_h = 0
+BBX_x = 0
+BBX_y = 0
+
+binaryChar = ""
+
+for line in lines:
+    if getpixsize.match(line):
+        pixsize = getpixsize.search(line).group(1)
+    
+    if getcharnum.match(line):
+        charnum = int(getcharnum.search(line).group(1))
+        if oldcharnum == 0:
+            firstcharnum = oldcharnum = charnum
+        if oldcharnum != charnum:
+            for i in range(int(oldcharnum)+1, int(charnum)):       #fill gap between non-consecutive chars
+                indexData += "\r\n\t{ RPLCEwithpropsoffillchar }, // " + str(i)
+            oldcharnum = charnum
+        
+    
+    if getName.match(line):
+        charName = getName.search(line).group(1)
+        
+    if getWidth.match(line):
+        advance = getWidth.search(line).group(1)
+        
+    if getBBX.match(line):
+        BBX_w = int(getBBX.search(line).group(1))
+        BBX_h = getBBX.search(line).group(2)
+        BBX_x = getBBX.search(line).group(3)
+        BBX_y = getBBX.search(line).group(4)
+    
+    if startBitmap.match(line):
+        onBitmapLine = 1
+    else:
+        if endBitmap.match(line):
+            if onBitmapLine != 0:  #We just finished reading a bitmap. Dump the binary cache!
+                bitmapData += "\r\n\t"
+                numbytes = int((len(binaryChar)+7)/8)    #How many bytes are required to store this bitmap?
+                #for i in range(0, len(binaryChar) % 8)
+                #    binaryChar += '0'                            #right-pad with zeros to finish a byte
+                binaryChar = binaryChar.ljust(numbytes*8, '0')  #right-pad with zeros to finish a byte
+                for i in range(numbytes):
+                    bitmapData += "0b"
+                    bitmapData += binaryChar[8*i:8*i+8]
+                    bitmapData += ", "
+                bitmapData += "// " + charName
+                                    #      bitmapOffset, width, height, xAdvance, xOffset, yOffset
+                indexData += "\r\n\t{   "
+                indexData += str(binaryIndex).rjust(3, ' ') + ", "      #bitmapOffset
+                indexData += str(BBX_w).rjust(3, ' ') + ", "      #width
+                indexData += str(BBX_h).rjust(3, ' ') + ", "      #height
+                indexData += str(advance).rjust(3, ' ') + ", "      #xAdvance
+                indexData += str(BBX_x).rjust(3, ' ') + ", "      #xOffset
+                BBX_y = -int(BBX_h)-int(BBX_y)
+                indexData += str(BBX_y).rjust(3, ' ') + ", "      #yOffset
+                indexData += " }, // " + charName
+                
+                binaryIndex += numbytes
+                binaryChar = ""
+
+            onBitmapLine = 0
+        else:
+            if onBitmapLine != 0:             #We are reading a bitmap!
+                
+                binaryChar += (bin(int(line, 16))[2:].zfill(len(line)*4-4))[:BBX_w]      
+               
+                #converts hex to binary with left-pad zeros, ltrimmed to BBX_w
+
+bitmapData += "\r\n};"
+indexData += "\r\n};"
+
+hfile = open(fontname + ".h", 'w')
+hfile.write(bitmapData + "\r\n\r\n" + indexData + "\r\nconst GFXfont " + fontname + " PROGMEM = {\r\n(uint8_t  *)" + fontname + "_Bitmap,(GFXglyph *)" + fontname + "_Glyphs, " + firstcharnum + ", " + charnum + ", " + pixsize + "};"
+)
+
